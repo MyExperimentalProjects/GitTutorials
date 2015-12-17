299fc269c0382e4fb89f417862db83de3b0059a0
Harshita Kasera <hkasera5@gmail.com>
2015-12-17 17:27:03 +0530

    Addel car model

--- GitBisect/Car.java
+++ GitBisect/Car.java
@@ -6,16 +6,18 @@ public class Car
     private double milesDriven;
     private double gasInTank;
     private double milesPerGallon;
+    private String model;
    
 
     /**
        Constructs a car with a given fuel efficiency.
     */
-    public Car(double mpg)
+    public Car(double mpg, String mdl)
     {
         milesDriven = 0;
         gasInTank = 0;
         milesPerGallon = mpg;
+        model = mdl;
     }
 
   
@@ -36,6 +38,15 @@ public class Car
     public double getGasInTank()
     {
         return gasInTank;
+    }
+
+    /**
+      Gets the model of this car.
+      @return the model
+    */
+    public String getModel()
+    {
+        return model;
     }
 
     /**
--- GitBisect/CarTest.java
+++ GitBisect/CarTest.java
@@ -2,7 +2,7 @@
 
 public class CarTest {
     public static void main(String args[]) {
-       Car c = new Car(10);
+       Car c = new Car(10,"Tesla");
        System.out.println(c.getGasInTank());
        c.addGas(10);
        c.drive(10);
