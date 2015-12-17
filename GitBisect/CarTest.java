a6039ae6005bc716e6a3fecf5b1aeeaf64fa023e
Harshita Kasera <hkasera5@gmail.com>
2015-12-17 17:23:58 +0530

    Added project initial content

---
+++ GitBisect/CarTest.java
@@ -0,0 +1,11 @@
+
+
+public class CarTest {
+    public static void main(String args[]) {
+       Car c = new Car(10);
+       System.out.println(c.getGasInTank());
+       c.addGas(10);
+       c.drive(10);
+       System.out.println(c.getGasInTank());
+    }
+}
